9b4622a49dcdcaecc19e5f5d17a3458322314711
Daniel Sel <daniel-sel@hotmail.com>
2020-10-22 22:25:16 +0200

    9

--- hack/test-e2e-run.sh
+++ hack/test-e2e-run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-.blob/kubetest --deployment=local --extract $(cat ./versions/kubernetes.txt) --test --check-version-skew=false --test_args="--ginkgo.focus=External.*Storage.* --storage.testdriver=./test/testdata/k8s-e2e-test-driver.yaml"
+cp -f ./test/testdata/k8s-e2e-test-driver.yaml /tmp/cryptpvc-csi-k8s-e2e-test-driver.yaml
+.blob/kubetest --deployment=local --extract $(cat ./versions/kubernetes.txt) --test --check-version-skew=false --test_args="--ginkgo.focus=External.*Storage.* --storage.testdriver=/tmp/cryptpvc-csi-k8s-e2e-test-driver.yaml"
