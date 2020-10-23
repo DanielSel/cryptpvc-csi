67d3f5c9e0fe2e69d21b130410882e2e01154812
Daniel Sel <daniel-sel@hotmail.com>
2020-10-24 01:19:30 +0200

    15

--- hack/test-e2e-run.sh
+++ hack/test-e2e-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 TESTDRIVER_PATH="$PWD/test/testdata/k8s-e2e-test-driver.yml"
 if [ ! -d "./kubernetes" ]; then
-    ./blob/kubetest --deployment=local --extract $(cat ./versions/kubernetes.txt) --test --check-version-skew=false --test_args="--ginkgo.focus=External.*Storage.* --storage.testdriver=${TESTDRIVER_PATH}"
+    .blob/kubetest --deployment=local --extract $(cat ./versions/kubernetes.txt) --test --check-version-skew=false --test_args="--ginkgo.focus=External.*Storage.* --storage.testdriver=${TESTDRIVER_PATH}"
 else
     (cd ./kubernetes && ../.blob/kubetest --deployment=local ${EXTRACT_ARGS} --test --check-version-skew=false --test_args="--ginkgo.focus=External.*Storage.* --storage.testdriver=${TESTDRIVER_PATH}")
 fi
